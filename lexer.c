b5b7877c1da2094b17adca88581f5d1d078389ac
brenolevi <brenolevi@ymail.com>
2022-04-04 00:44:49 -0300

    Create file path string

--- lexer.c
+++ lexer.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+static const char FILE_PATH[] = "examples/return_3.c";
+
 int main(void)
 {
     FILE *file;
     int i = 0;
 
-    file = fopen("examples/return_3.c", "r");
+    file = fopen(FILE_PATH, "r");
 
     while (1) {
         i = fgetc(file);
