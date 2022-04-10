4bb245120493dfd6385edd1e63acbb680bc86a30
brenolevi <brenolevi@ymail.com>
2022-04-09 22:06:30 -0300

    Working lexer for all valid/invalid cases

--- lexer.py
+++ lexer.py
@@ -1,11 +1,11 @@
-FILE_PATH = "examples/valid/return_3.c"
+FILE_PATH = "examples/valid/multi_digit.c"
 
 
-def is_space_char(c):
+def is_space(c):
     return c.isspace()
 
 
-def is_special_char(c):
+def is_special(c):
     return c in ['(', ')', '{', '}', '[', ']', ',', ';', '+', '-']
 
 
@@ -17,9 +17,16 @@ def is_letter(c):
     return c.isalpha()
 
 
+def is_double_quotes(c):
+    return c == '"'
+
+
+def is_underscore(c):
+    return c == '_'
+
 def tokenize(file_path):
-    tokens = []
     flat = ""
+
     with open(file_path, 'r') as file:
         for line in file:
             flat += line
@@ -27,30 +34,77 @@ def tokenize(file_path):
     in_digit = False
     in_quotes = False
     in_identifier = False
-    constructing_token = ""
+
+    tokens = []
+    _token = ""
+
     for i in range(len(flat)):
         c = flat[i]
         if in_digit:
-            pass
+            if is_digit(c) or is_letter(c):
+                _token += c # lexical errors will be treated later
+            elif is_space(c):
+                tokens.append(_token)
+                _token = ""
+                in_digit = False
+            elif is_double_quotes(c):
+                tokens.append(_token)
+                _token = c
+                in_digit = False
+                in_quotes = True
+            elif is_special(c):
+                tokens.append(_token)
+                _token = ""
+                tokens.append(c)
+                in_digit = False
+            else:
+                raise ValueError("Tokenizer (in-digit) got an unidentified character!")
         elif in_quotes:
-            pass
+            if is_double_quotes(c):
+                _token += c
+                tokens.append(_token)
+                _token = ""
+                in_quotes = False
+            else:
+                _token += c
         elif in_identifier:
-            pass
+            if is_digit(c) or is_letter(c) or is_underscore(c):
+                _token += c
+            elif is_space(c):
+                tokens.append(_token)
+                _token = ""
+                in_identifier = False
+            elif is_double_quotes(c):
+                tokens.append(_token)
+                _token = c
+                in_identifier = False
+                in_quotes = True
+            elif is_special(c):
+                tokens.append(_token)
+                _token = ""
+                tokens.append(c)
+                in_identifier = False
+            else:
+                raise ValueError("Tokenizer (in-identifier) got an unidentified character!")
         else:
-            if is_space_char(c):
+            if is_space(c):
                 pass
             elif is_digit(c):
                 in_digit = True
-                constructing_token += c
-            elif c == '"':
+                _token += c
+            elif is_double_quotes(c):
                 in_quotes = True
-                constructing_token += c
-            elif is_letter(c):
+                _token += c
+            elif is_letter(c) or is_underscore(c):
                 in_identifier = True
-                constructing_token += c
-            elif is_special_char(c):
+                _token += c
+            elif is_special(c):
                 tokens.append(c)
+            else:
+                raise ValueError("Tokenizer (first-digit) got an unidentified character!")
 
+    if _token != "":
+        tokens.append(_token)
     return tokens
 
 
