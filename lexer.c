c1002a8e29fbd8985874024e3e0ff16fe4c771e8
brenolevi <brenolevi@ymail.com>
2022-04-12 00:17:47 -0300

    Create linked list of tokens

--- lexer.c
+++ lexer.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define BUFFER_SIZE 4096
 
 static const char FILE_PATH[] = "examples/valid/return_3.c";
 
@@ -17,10 +20,40 @@ enum TokenType {
     UNKNOWN
 };
 
+typedef struct token_data {
+    char *token_text;
+    int token_type;
+    struct token_data *next;
+} Token;
+
+Token *createToken(char *s) {
+    Token *newToken = (Token *)malloc(sizeof(Token));
+    newToken->token_text = s;
+    newToken->next = NULL;
+    return newToken;
+}
+
+
+void insert(Token **link, Token *newToken) {
+    newToken->next = *link;
+    *link = newToken;
+}
+
+void printTokens(Token *head) {
+    while (head != NULL) {
+        printf("%s  ->  %d\n", head->token_text, head->token_type);
+        head = head->next;
+    }
+}
+
 int main(void)
 {
     FILE *file;
     char c;
+
+    Token *head = NULL;
+    Token *tail = NULL;
+    Token *n;
 
     file = fopen(FILE_PATH, "r");
 
